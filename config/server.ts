510ecbbde4b55f958295ac79bd9f6b6d0c520a93
Kamesh A S <kamesheas@gmail.com>
2025-10-16 19:44:08 +0530

    Server File Change added Proxy

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,6 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('RENDER_EXTERNAL_URL', 'https://dreamknottbyad-strapi.onrender.com'),
+  proxy: true, 
 });
