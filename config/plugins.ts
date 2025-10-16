08d637786feaad98e7f71c11801798c2f1f9e669
Kamesh A S <kamesheas@gmail.com>
2025-10-16 18:48:32 +0530

    Initial Strapi Setup

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default {
+  'cloudinary-media-library': {
+    enabled: true,
+    config: {
+      cloudName: 'DreamKnot',
+      apiKey: '154841424944738',
+      encryptionKey: 'fC_rGiwzR2K3HY-Q36BamMcTadA',
+    },
+  },
+};
